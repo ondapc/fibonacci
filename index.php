58f2eb6d769e94ceeb9f310a89eefc97470efa33
ondapc <admin@ondapc.com>
2017-03-16 18:26:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('functions.php');
-$fib = (isset($_REQUEST['f']) && $_REQUEST['f']) ? $_REQUEST['f'] : '15';
+$fib = (isset($_REQUEST['f']) && $_REQUEST['f']) ? $_REQUEST['f'] : '20';
+if ( !is_numeric($fib) ) { $fib = "2"; }
 ?>
 <!DOCTYPE html>
 <html>
